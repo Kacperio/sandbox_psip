389376885a734de5e117462f441d5fcc6f10b2a9
Kacperio <149382355+Kacperio@users.noreply.github.com>
2023-10-30 14:45:45 +0100

    funkcjonal_odczyt

---
+++ dane.py
@@ -0,0 +1,13 @@
+
+
+users_list = [
+    {"name":"Kacperek", "nick":"hajdun", "posts":47},
+    {"name":"Jurek", "nick":"Juraso", "posts":47},
+    {"name":"Elizabeth", "nick":"una", "posts":47},
+    {"name":"adma", "nick":"kloc", "posts":47},
+    {"name":"kasia", "nick":"woker", "posts":47},
+    {"name":"tom", "nick":"ajdu", "posts":47},
+    {"name":"elojzy", "nick":"reto", "posts":47},
+    {"name":"tadek", "nick":"vibe", "posts":47},
+    {"name":"monik", "nick":"lelo", "posts":47}
+    ]
---
+++ main.py
@@ -0,0 +1,10 @@
+
+from  dane import users_list
+
+
+for user in users_list:
+    print(user['nick'], 'dodal tyle ', user['posts'], 'postuf')
+
+
+
+# print(f'dodal {dane[0]["nick"]} tyle {dane[0]["posts"]} postuf')
