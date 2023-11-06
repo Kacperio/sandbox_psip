c8845a6a7c3da46f4939b6ccfbeb41160ad36000
Kacperio <149382355+Kacperio@users.noreply.github.com>
2023-11-06 14:49:19 +0100

    chore: add gui

--- main.py
+++ main.py
@@ -1,49 +1,5 @@
 from  dane import users_list
+from utils import GUI
 
-tp_list = []
-
-def add_user_to(users_list:list) -> None:
-    name = input('Pod1aj imię: ')
-    nick = input('Podaj ksyweczke: ')
-    post = int(input('Ile wstawił postuf: '))
-    users_list.append({"name": name, "nick": nick, "posts": post})
-
-def remove_user_from(users_list:list) -> None:
-    name = input('kogo chcesz wykopac :')
-    for user in users_list:
-        if user['name']== name:
-            tp_list.append(user)
-    print('znaleziono takich uzytkownikuw:')
-    print('0 usuwa wszystkich')
-    for numerek, user_to_be_removed in enumerate(tp_list):
-        print(numerek+1, user_to_be_removed)
-    numer = int(input('wybierz kościa do wyautowania: '))
-    if numer == 0:
-        for user in tp_list:
-            users_list.remove(user)
-    else:
-        users_list.remove(tp_list[numer-1])
-
-    # print(numer)
-    # print(tp_list[numer-1])
-
-
-remove_user_from(users_list)
-
-
-# l_fraj = int(input('Ilu chcesz dodać frajeruf: '))
-
-# for x in range(l_fraj):
-#     add_user_to(users_list)
-
-
-# for x in range(l_fraj):
-#     name = input('Pod1aj imię: ')
-#     nick = input('Podaj ksyweczke: ')
-#     post = int(input('Ile wstawił postuf: '))
-#     users_list.append({"name": name, "nick": nick, "posts": post})
-
-for user in users_list:
-    # print(user['nick'], 'dodal tyle ', user['posts'], 'postuf')
-    print(f'dodal {user["name"]} tyle {user["posts"]} postuf')
+GUI(users_list)
 
