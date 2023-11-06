778a43f74d205b9fadb16b98705e051d39c273f2
Kacperio <149382355+Kacperio@users.noreply.github.com>
2023-11-06 09:23:37 +0100

    test-add
    new user - test

--- dane.py
+++ dane.py
@@ -6,8 +6,9 @@ users_list = [
     {"name":"Elizabeth", "nick":"una", "posts":47},
     {"name":"adma", "nick":"kloc", "posts":47},
     {"name":"kasia", "nick":"woker", "posts":47},
-    {"name":"tom", "nick":"ajdu", "posts":47},
-    {"name":"elojzy", "nick":"reto", "posts":47},
-    {"name":"tadek", "nick":"vibe", "posts":47},
-    {"name":"monik", "nick":"lelo", "posts":47}
+    {"name":"tom", "nick":"ajdu", "posts":89},
+    {"name":"elojzy", "nick":"reto", "posts":37},
+    {"name":"tadek", "nick":"vibe", "posts":27},
+    {"name":"monik", "nick":"lelo", "posts":49},
+    {"name":"agetke", "nick":"dzala", "posts":54}
     ]
