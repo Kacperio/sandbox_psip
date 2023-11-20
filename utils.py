6b435a612a6d111f1b975711765ab4741780d12f
Oliverek-spec <149382623+Oliverek-spec@users.noreply.github.com>
2023-11-20 13:29:03 +0100

    added new update function

--- utils.py
+++ utils.py
@@ -54,10 +54,20 @@ def GUI(users_list):
                 remove_user_from(users_list)
             case 4:
                 print('modyfikacjion')
-                print('jeszcze ni mo')
+                update_user(users_list)
 
-
-
+def update_user(users_list: list[dict, dict]) -> None:
+    nick_of_user = input('Podaj nick użytkownika do modyfikacji ')
+    print(f'Wpisano {nick_of_user}')
+    for user in users_list:
+        if user['nick'] == nick_of_user:
+            print('znaleziono')
+            new_name = input('Podaj nowe imię użytkownika ')
+            user['name'] = new_name
+            new_nick = input('Podaj nowy nick użytkownika ')
+            user['nick'] = new_nick
+            new_posts = input('Podaj nową ilość postów ')
+            user['posts'] = new_posts
 # l_fraj = int(input('Ilu chcesz dodać frajeruf: '))
 
 # for x in range(l_fraj):
