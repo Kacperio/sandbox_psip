7b3ea5dca2f9b610cd64497d454ddce70fe7417d
Kacperio <149382355+Kacperio@users.noreply.github.com>
2023-11-27 15:01:32 +0100

    vip-map to gui
    sprawdzic 5

--- notatki.py
+++ notatki.py
@@ -4,6 +4,7 @@ import re
 import folium
 
 
+user = {'city':"Kutno", "name":"Kacperek", "nick":"hajdun", "posts":12}
 miasteczka = ['Kutno', 'Warszawa', 'Zamość']
 # city = input('Podaj nazwę jakiejś dziury: ')
 # city = 'Kutno'
@@ -18,21 +19,48 @@ def get_coords(city:str)->list[float,float]:
     res_h_lat = response_htlm.select('.latitude')[1].text # . bo class
     res_h_lat = float(res_h_lat.replace(',','.'))
 
-    # print(res_h_lat[23:-7])
-    # lt = re.sub("(\<).*?(\>)", repl='', string=res_h_lat, count=0, flags=0)
-
     res_h_lon = response_htlm.select('.longitude')[1].text # . bo class
     res_h_lon = float(res_h_lon.replace(',','.'))
 
     return [res_h_lat,res_h_lon]
 
-# for xx in miasteczka:
-#     print(get_coords(xx))
 
-siti = get_coords(city='Kutno')
-mapa = folium.Map(location=siti, tiles='OpenStreetMap', zoom_start=7)
+def get_map_of_single(user:str):
+    siti = get_coords(user['city'])
 
-for xx in miasteczka:
-    folium.Marker(location=get_coords(xx), popup="HALABARDAAAA").add_to(mapa)
+    mapa = folium.Map(location=siti, tiles='OpenStreetMap', zoom_start=14)
+    folium.Marker(location=siti, popup=f"HALABARDAAAA\n{user['name']}").add_to(mapa)
 
-mapa.save('mapka.html')
+    mapa.save(f'mapka{user["name"]}.html')
+
+def get_map_of(coords: list, lista_imion):
+    mapa = folium.Map(location=[52.3, 21.0] , tiles='OpenStreetMap', zoom_start=7)
+    
+    for user in users:
+        aa = get_coords(city=user['city'])
+        folium.Marker(location=aa, popup=f"HALABARDAAAA\n{user['name']}").add_to(mapa)
+    mapa.save(f'mapeeeczka.html')
+
+from dane import users_list
+
+get_map_of(users_list)
+
+    # siti = get_coords(user['city'])
+    # mapa = folium.Map(location=get_coords(siti), tiles='OpenStreetMap', zoom_start=7)
+
+    # for xx in miasteczka:
+    #     folium.Marker(location=get_coords(siti), popup="HALABARDAAAA").add_to(mapa)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
